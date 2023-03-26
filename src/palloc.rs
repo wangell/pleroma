5ca860f9d211875bf2e1cf65223f73c029beed93
William H.S. Angell <william.h.angell@gmail.com>
2023-03-26 20:43:25 +0200

    Increase stack + heap size

--- src/palloc.rs
+++ src/palloc.rs
@@ -7,7 +7,7 @@ use x86_64::{PhysAddr, VirtAddr};
 use linked_list_allocator::LockedHeap;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 1024 * 1024;
+pub const HEAP_SIZE: usize = 1024 * 1024 * 2;
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
