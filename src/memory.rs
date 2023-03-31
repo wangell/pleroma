d6fece8b88f7e1dc9a18bf574ef7eb8e785aead2
William H.S. Angell <william.h.angell@gmail.com>
2023-03-31 20:22:11 +0200

    Add bitmap frame allocator

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,11 @@
-use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PhysFrame, Size4KiB, OffsetPageTable, PageTable, Translate};
-use x86_64::{PhysAddr, VirtAddr};
+use limine::LimineMemmapEntry;
 use limine::LimineMemmapRequest;
+use limine::LimineMemoryMapEntryType;
+use limine::NonNullPtr;
+use x86_64::structures::paging::{
+    FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB, Translate,
+};
+use x86_64::{PhysAddr, VirtAddr};
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
@@ -21,32 +26,18 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
 
 pub struct BootInfoFrameAllocator {
     next: usize,
+    max_entries: usize,
 }
 
+static mut bitmap: [u8; 50000] = [0; 50000];
 static MM_INFO: LimineMemmapRequest = LimineMemmapRequest::new(0);
 
 unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
     fn allocate_frame(&mut self) -> Option<PhysFrame> {
-        use limine::LimineMemoryMapEntryType;
-        let mmap = MM_INFO
-            .get_response()
-            .get()
-            .expect("failed to get memory map")
-            .memmap();
-        let mut usable_frames = mmap
-            .iter()
-            .filter(|entry| entry.typ == LimineMemoryMapEntryType::Usable)
-            .map(|area| {
-                let frame_addr = area.base;
-                let frame_end = area.base + area.len;
-                let frame_size = frame_end - frame_addr;
-                let num_frames = frame_size / 4096;
-                let start_frame = PhysFrame::containing_address(PhysAddr::new(frame_addr));
-                (0..num_frames).map(move |i| start_frame + i)
-            })
-            .flatten();
-        let frame = usable_frames.nth(self.next).clone();
-        self.next += 1;
+
+        let frame_idx = self.get_contiguous_range(1).0 as u64;
+        let frame = Some(PhysFrame::from_start_address(PhysAddr::new(frame_idx * 4096)).unwrap());
+        self.mark_page_used(frame_idx as usize);
 
         frame
     }
@@ -54,6 +45,123 @@ unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
 
 impl BootInfoFrameAllocator {
     pub unsafe fn init() -> Self {
-        Self { next: 0 }
+        let mut bifa = Self {
+            next: 0,
+            max_entries: 0
+        };
+
+        let mmap = MM_INFO
+            .get_response()
+            .get()
+            .expect("failed to get memory map")
+            .memmap();
+
+        unsafe {
+            // Assume all frames are used
+            for i in 0..bitmap.len() {
+                bitmap[i] = 0xFF;
+            }
+
+            for area in mmap
+                .iter()
+                .filter(|entry| entry.typ == LimineMemoryMapEntryType::Usable)
+            {
+                let frame_start = area.base / 4096;
+                let frame_end = (area.base + area.len) / 4096;
+
+                // Mark until one page before frame_end
+                for q in frame_start..frame_end {
+                    bifa.mark_page_unused(q as usize);
+                }
+            }
+        }
+
+        bifa
+    }
+
+    fn get_contiguous_range(&mut self, n: u64) -> (u64, u64) {
+        // [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1]
+        let mut n_start = 0;
+        let mut n_end = 0;
+
+        if (n == 0) {
+            panic!();
+        }
+
+        while (!self.is_page_free(n_start)) {
+            n_start = n_start + 1;
+        }
+
+        n_end = n_start;
+
+        while (n_end - n_start + 1) < n as usize {
+            if self.is_page_free((n_end + 1) as usize) {
+                n_end += 1;
+            } else {
+
+                n_start = n_end + 1;
+
+                while !self.is_page_free(n_start) {
+                    n_start = n_start + 1;
+                }
+
+                n_end = n_start;
+            }
+
+            unsafe {
+                if n_end > bitmap.len() as usize {
+                    panic!()
+                };
+            }
+        }
+
+        (n_start as u64, n_end as u64)
+    }
+
+    pub fn is_page_free(&mut self, page_n: usize) -> bool {
+        let base_idx = page_n / 8;
+        let offset_idx = page_n % 8;
+
+        unsafe {
+            (bitmap[base_idx] >> (7 - offset_idx) & 1) == 0
+        }
+    }
+
+    pub fn mark_page_used(&mut self, page_n: usize) {
+        let base_idx = page_n / 8;
+        let offset_idx = page_n % 8;
+
+        unsafe {
+            if (!self.is_page_free(page_n)) {
+                panic!("Trying to mark an already used page: {}!", page_n);
+            }
+
+            bitmap[base_idx] |= (1 << (7 - offset_idx));
+        }
+    }
+
+    pub fn mark_page_unused(&mut self, page_n: usize) {
+        let base_idx = page_n / 8;
+        let offset_idx = page_n % 8;
+
+        unsafe {
+            bitmap[base_idx] &= !(1 << (7 - offset_idx));
+        }
     }
 }
+
+pub trait BootInfoAllocatorTrait {
+    fn allocate_contiguous(&mut self, n: u64) -> PhysFrame;
+}
+
+impl BootInfoAllocatorTrait for BootInfoFrameAllocator {
+    fn allocate_contiguous(&mut self, n: u64) -> PhysFrame {
+        let (start, end) = self.get_contiguous_range(n);
+        for i in start..=end {
+            self.mark_page_used(i as usize);
+        }
+
+        PhysFrame::from_start_address(PhysAddr::new(start * 4096)).unwrap()
+    }
+}
+
--- src/palloc.rs
+++ src/palloc.rs
@@ -1,18 +1,21 @@
 extern crate alloc;
 use alloc::alloc::{GlobalAlloc, Layout};
 use x86_64::structures::paging::{
-    mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
+    mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB, PhysFrame
 };
 use x86_64::{PhysAddr, VirtAddr};
 use linked_list_allocator::LockedHeap;
+use crate::memory::BootInfoAllocatorTrait;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 1024 * 1024 * 2;
+pub const HEAP_SIZE: usize = 1024 * 1024 * 50;
 
-pub fn init_heap(
+pub fn init_heap<T>(
     mapper: &mut impl Mapper<Size4KiB>,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) -> Result<(), MapToError<Size4KiB>> {
+    frame_allocator: &mut T,
+) -> Result<(), MapToError<Size4KiB>>
+    where T: FrameAllocator<Size4KiB> + BootInfoAllocatorTrait,
+{
     let page_range = {
         let heap_start = VirtAddr::new(HEAP_START as u64);
         let heap_end = heap_start + HEAP_SIZE - 1u64;
@@ -21,11 +24,15 @@ pub fn init_heap(
         Page::range_inclusive(heap_start_page, heap_end_page)
     };
 
-    for page in page_range {
-        let frame = frame_allocator
-            .allocate_frame()
-            .ok_or(MapToError::FrameAllocationFailed)?;
+    // Memory should be relatively untouched at this point and should have a large contiguous space
+    let heap_size = ((HEAP_START as u64) + (HEAP_SIZE as u64) - 1u64) - HEAP_START as u64;
+    let n_frames = (heap_size / 4096) + 1;
+    // FIXME: this should fallback to single frame allocation on error
+    let frame_start = frame_allocator.allocate_contiguous(n_frames);
+
+    for (idx, page) in page_range.enumerate() {
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+        let frame = PhysFrame::from_start_address(PhysAddr::new(frame_start.start_address().as_u64() + (idx as u64 * 4096))).unwrap();
         unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
     }
 
